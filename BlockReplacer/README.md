77b245d3d77a640cc4d845f3e45dcec3c39cce4d
Matej <matejdro@gmail.com>
2015-03-01 19:05:29 +0100

    BlockReplacer

---
+++ BlockReplacer/README.md
@@ -0,0 +1,22 @@
+# BlockReplacer #
+
+A command line tool working on region files (containing chunk data). It replaces all blocks of specificed types with another specified block type, in defined height range.
+
+## Usage ##
+
+```
+java -jar BlockReplacer.jar
+    <region folder> <minY> <maxY> <original blocks ID> <replacement block ID>
+```
+
+## Example ##
+
+```
+java -jar BlockReplacer.jar region 110 10,11 9
+```
+
+Above example will work on the `region` folder – the `BlockReplacer.jar` file was placed in `DIM-1` folder (Nether).
+
+The example will replace all flowing lava blocks (ID = `10`) and stationary lava blocks (ID = `11`) between Y `110` and `117` (inclusive) with flowing water blocks (ID = `8`).
+
+It works on all `.mca` files in the `region` folder so make sure you pre-generate all chunks in the first place. Use [WorldBorder](http://dev.bukkit.org/bukkit-plugins/worldborder/) or [Minecraft Land Generator](https://sites.google.com/site/minecraftlandgenerator/).
---
+++ BlockReplacer/src/com/matejdro/BlockReplacer/Main.java
@@ -0,0 +1,159 @@
+package com.matejdro.BlockReplacer;
+
+import com.mojang.nbt.CompoundTag;
+import com.mojang.nbt.ListTag;
+import com.mojang.nbt.NbtIo;
+import java.util.HashSet;
+import java.util.List;
+import net.minecraft.world.level.chunk.storage.RegionFile;
+
+import java.io.*;
+import java.sql.*;
+
+/**
+ * Created by Matej on 29.5.2014.
+ */
+public class Main
+{
+    private static int minY;
+    private static int maxY;
+    private static HashSet<Integer> sourceBlockIDs;
+    private static int targetBlockID;
+
+    public static void main(String[] args)
+    {
+        if (args.length < 4)
+        {
+            System.out.println("java -jar BlockReplacer.jar <region folder> <minY> <maxY> <original blocks ID> <replacement block ID>");
+            return;
+        }
+
+
+        File regionFolder = new File(args[0]);
+        minY = Integer.parseInt(args[1]);
+        maxY = Integer.parseInt(args[2]);
+        String sourceBlockList = args[3];
+        targetBlockID = Integer.parseInt(args[4]);
+
+        sourceBlockIDs = new HashSet<>();
+        String[] splitBlockIDs = sourceBlockList.split(",");
+        for (String id : splitBlockIDs)
+            sourceBlockIDs.add(Integer.parseInt(id));
+
+        processWorldFolder(regionFolder);
+    }
+    public static void processWorldFolder(File folder)
+    {
+        System.out.println("Loading chunks from " + folder.getAbsolutePath() + " ...");
+
+        FileFilter filter = new FileFilter()
+        {
+            @Override
+            public boolean accept(File file)
+            {
+                return file.getName().endsWith(".mca");
+            }
+        };
+
+        for (File file : folder.listFiles(filter))
+        {
+            processRegion(file);
+        }
+    }
+
+    public static void processRegion(File file)
+    {
+        System.out.println("    Processing region " + file.getName() + " ...");
+        try
+        {
+            RegionFile region = new RegionFile(file);
+            for (int x = 0; x < 32; x++)
+            {
+                for (int z = 0; z < 32; z++)
+                {
+                    if (!region.hasChunk(x, z))
+                        continue;
+
+
+                    DataInputStream chunkStream = region.getChunkDataInputStream(x, z);
+                    CompoundTag chunk = NbtIo.read(chunkStream);
+                    chunkStream.close();
+
+                    CompoundTag levelTag = chunk.getCompound("Level");
+                    ListTag sectionsTag = levelTag.getList("Sections");
+
+                    boolean chunkChanged = false;
+
+                    for (int i = 0; i < sectionsTag.size(); i++)
+                    {
+                        CompoundTag section = (CompoundTag) sectionsTag.get(i);
+                        int sectionY = section.getByte("Y");
+                        byte[] blocksArray = section.getByteArray("Blocks");
+
+                        boolean sectionChanged = false;
+
+                        for (int j = 0; j < blocksArray.length; j++)
+                        {
+                            //int blockX = j % 16;
+                            //int blockZ = (j / 16) % 16;
+                            int blockY = (j / 256) % 16;
+
+                           // int test = blockY * 16 * 16 + blockZ * 16 + blockX;
+
+                            blockY += sectionY * 16;
+
+                            if (blockY >= minY && blockY <= maxY && sourceBlockIDs.contains(Byte.toUnsignedInt(blocksArray[j])))
+                            {
+                                blocksArray[j] = (byte) targetBlockID;
+                                sectionChanged = true;
+                            }
+                        }
+
+                        if (sectionChanged)
+                        {
+                            chunkChanged = true;
+                            section.putByteArray("Blocks", blocksArray);
+                        }
+                    }
+
+                    if (chunkChanged)
+                    {
+                        chunk.put("Sections", sectionsTag);
+
+                        DataOutputStream chunkOutputStream = region.getChunkDataOutputStream(x, z);
+                        NbtIo.write(chunk, chunkOutputStream);
+                        chunkOutputStream.close();
+                    }
+                 }
+            }
+
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static Connection openConnection(String path)
+    {
+
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            Connection ret = DriverManager.getConnection("jdbc:sqlite:" + path);
+            ret.setAutoCommit(false);
+            return ret;
+
+        } catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
